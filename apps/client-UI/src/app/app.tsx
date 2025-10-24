ca46f186499c4129abeb04fb204fab592aaf6a8b
aleexxii <amaleexxii@gmail.com>
2025-10-24 21:43:55 +0530

    added fonts, images for the first landing page

--- apps/client-UI/src/app/app.tsx
+++ apps/client-UI/src/app/app.tsx
@@ -1,9 +1,10 @@
+import Landing from "./pages/Landing/Landing";
 
 
 export function App() {
   return (
     <div className="text-center justify-center">
-      <h1 className="text-3xl underline bg-red-400"> WELCOME TO CLIENT UI </h1>
+      <Landing />
     </div>
     
   );
