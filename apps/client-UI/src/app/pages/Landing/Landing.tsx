46daa7bf751f418b914cc72f0464e20873fbd47c
aleexxii <amaleexxii@gmail.com>
2025-11-13 15:37:58 +0530

    added learningvideo, puzzle challenge section

--- apps/client-UI/src/app/pages/Landing/Landing.tsx
+++ apps/client-UI/src/app/pages/Landing/Landing.tsx
@@ -1,9 +1,13 @@
 import Hero from "./sections/Hero"
+import LearningVideosSection from "./sections/LearningVideos"
+import PuzzleChallenge from "./sections/PuzzleChallenge"
 
 function Landing() {
   return (
     <div>
         <Hero />
+        <LearningVideosSection />
+        <PuzzleChallenge />
     </div>
   )
 }
