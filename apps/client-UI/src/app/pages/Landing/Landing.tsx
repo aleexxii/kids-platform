7a03ee0cb5c44be0aeb2bce0b8aada93265fc6a0
aleexxii <amaleexxii@gmail.com>
2025-12-24 12:55:52 +0530

    refactor: rename Showcase to LearningPaths

--- apps/client-UI/src/app/pages/Landing/Landing.tsx
+++ apps/client-UI/src/app/pages/Landing/Landing.tsx
@@ -1,13 +1,13 @@
 import Hero from "./sections/Hero"
 import About from "./sections/About"
-import PuzzleChallenge from "./sections/Showcase"
+import LearninPaths from "./sections/LearningPaths"
 
 function Landing() {
   return (
     <div>
         <Hero />
         <About />
-        <PuzzleChallenge />
+        <LearninPaths />
     </div>
   )
 }
