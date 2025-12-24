3320e7a73322cbaf480d2390e5daf5d27aab4c16
aleexxii <amaleexxii@gmail.com>
2025-12-24 12:20:10 +0530

    feat: add user routes

--- apps/client-UI/src/app/routes/User.Routes.tsx
+++ apps/client-UI/src/app/routes/User.Routes.tsx
@@ -1,10 +1,33 @@
-import { createBrowserRouter } from 'react-router'
-import Landing from '../pages/Landing/Landing'
+import { createBrowserRouter } from 'react-router';
+import Landing from '../pages/Landing/Landing';
+import Login from '../pages/auth/Login';
+import SignUp from '../pages/auth/SignUp';
+import {
+  About,
+  ForParents,
+  LearningPaths,
+  Contact,
+  HowItWorks,
+} from '../pages/Landing/sections';
 
 export const clientRouter = createBrowserRouter([
-    {
-        path : '/',
-        element : <Landing />
-    },
-
-])
+  {
+    path: '/',
+    element: <Landing />,
+    children: [
+      { path: 'about', element: <About /> },
+      { path: 'parents', element: <ForParents /> },
+      { path: 'programs', element: <LearningPaths /> },
+      { path: 'contact', element: <Contact /> },
+      { path: 'how-it-works', element: <HowItWorks /> },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+]);
